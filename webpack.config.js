e0541bf23ccb65ec96a15049ad5541f69604a82f
LLath <rothpatrick12@yahoo.de>
2020-05-10 00:38:20 +0200

    feat: connect react

---
+++ webpack.config.js
@@ -0,0 +1,97 @@
+const path = require("path");
+// const dotenv = require("dotenv");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+// const FaviconsWebpackPlugin = require("favicons-webpack-plugin");
+const { HashedModuleIdsPlugin } = require("webpack");
+// const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
+// .BundleAnalyzerPlugin;
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "[name].[hash].js",
+    publicPath: "/",
+    chunkFilename: "[name].[hash].js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.(jpe?g|gif|png|svg|woff|ttf|wav|mp3|bmp)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              esModule: false,
+              publicPath: "/",
+            },
+          },
+        ],
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.ext$/,
+        use: ["cache-lodaer", "css-loader", "file-loader"],
+        include: path.resolve("src"),
+      },
+    ],
+  },
+  devtool: "eval-source-map",
+  optimization: {
+    runtimeChunk: "single",
+    splitChunks: {
+      chunks: "all",
+      maxInitialRequests: Infinity,
+      minSize: 0,
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name(module) {
+            const packageName = module.context.match(
+              /[\\/]node_modules[\\/](.*?)([\\/]|$)/
+            )[1];
+            return `npm.${packageName.replace("@", "")}`;
+          },
+        },
+      },
+    },
+  },
+  resolve: {
+    /**
+     * @todo place relative paths here
+     */
+    alias: {
+      Assets: path.resolve(__dirname, "src/Assets/"),
+      Settings: path.resolve(__dirname, "./settings.js"),
+    },
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, "build"),
+    hot: true,
+    historyApiFallback: true,
+    compress: true,
+    index: "index.html",
+  },
+  plugins: [
+    // new DefinePlugin({
+    //   "process.env": JSON.stringify(dotenv.config().parsed),
+    // }),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+    // new FaviconsWebpackPlugin({
+    //   logo: "./src/Assets/icons/favicon.png",
+    // }),
+    new HashedModuleIdsPlugin(), // so that file hashes don't change unexpectedly
+  ],
+};
